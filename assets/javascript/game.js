210c08766130a5afdf671487aba615740dbe9687
Michael Vega <ichigo509@yahoo.com>
2019-06-21 13:22:11 -0400

    Initial pseudocode for html and js, also linked css and js

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+//declare wins, losses, guesses left, and letters chosen variables
+
+//make an array of letters for choices and a variable for the compChoice
+
+//make function that whenever the game starts the computer chooses a random letter
+
+//make a function that whenever a letter is pressed if it does not match the compChoice remove a point from guesses left, else increase wins by 1 and compChoice gets reset
+
+//make a function that if guesses left reaches 0 losses increases by one and the compChoice gets reset with a new letter
